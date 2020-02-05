f2a1580f4e25088ed95c4dd428835f4be3699c8d
Ben Remer <benaremer@gmail.com>
2020-02-04 20:26:01 -0500

    finished phase c

--- kern/src/main.rs
+++ kern/src/main.rs
@@ -18,8 +18,38 @@ use console::kprintln;
 
 // FIXME: You need to add dependencies here to
 // test your drivers (Phase 2). Add them as needed.
+use pi::timer::*;
+use core::time::Duration;
+use pi::gpio::Gpio;
 
-fn kmain() -> ! {
+
+unsafe fn kmain() -> ! {
     // FIXME: Start the shell.
-    unimplemented!()
+    blink();
+}
+
+unsafe fn blink() -> ! {
+    // let mut output = Gpio::new(16).into_output();
+    let mut leds = [
+        Gpio::new(5).into_output(),
+        Gpio::new(6).into_output(),
+        Gpio::new(13).into_output(),
+        Gpio::new(19).into_output(),
+        Gpio::new(26).into_output(),
+    ];
+    let dur = Duration::new(1,0);
+    loop {
+        for mut led in leds.iter_mut() {
+            led.set();
+            spin_sleep(dur);
+            led.clear();
+            spin_sleep(dur);
+        }
+    }
+    // loop {
+    //     output.set();
+    //     spin_sleep(dur);
+    //     output.clear();
+    //     spin_sleep(dur);
+    // }
 }
